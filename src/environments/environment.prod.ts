c4f0603171bb1ccfe9f3c9d4a6e4b4b75d513ad0
Daniel Garcia Rio <daniel.garcia.rio@gmail.com>
2020-07-03 09:39:34 +0200

    Cambios en los usuarios

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -9,10 +9,10 @@ export const lagersdef: Array<{ cwar: string; lager: string }> = [
   { cwar: 'BM1', lager: 'Berlin Mietlager' },
   { cwar: 'R01', lager: 'Rostock Haupt Lager' },
   { cwar: 'R02', lager: 'Rostock Montagewagen' },
-  { cwar: 'RM1', lager: 'Rostock Mietlager' }
+  { cwar: 'RM1', lager: 'Rostock Mietlager' },
 ];
 
-export const beobenutzer: Array<string> = ['444', '167', '346', '361'];
+export const beobenutzer: Array<string> = ['444', '167', '19', '361'];
 
 export const environment = {
   production: true,
@@ -21,5 +21,5 @@ export const environment = {
   tokenaddress: 'stock_perminv_jwtToken',
   lagers: lagersdef,
   beowachlager: 'S36',
-  beousers: beobenutzer
+  beousers: beobenutzer,
 };
